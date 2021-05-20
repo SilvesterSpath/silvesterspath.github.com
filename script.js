d1feb1f1ee1c2b145bba8db82d2da7082b492106
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-05-20 05:21:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,50 @@
-const open = document.getElementById('open');
-const close = document.getElementById('close');
-const container = document.querySelector('.container');
+const boxes = document.querySelectorAll('.box');
 
-open.addEventListener('click', () => {
-  container.classList.add('show-nav');
-});
+const links = [
+  'https://silvesterspath.github.io/github_finder/',
+  'https://silvesterspath.github.io/Incrementing-Counter/',
+  'https://silvesterspath.github.io/Animated-Navigation/',
+  'https://silvesterspath.github.io/Random-Picker/',
+  'https://silvesterspath.github.io/How-many-Drinks/',
+  'repa',
+  'retek',
+];
 
-close.addEventListener('click', () => {
-  container.classList.remove('show-nav');
-});
+const images = [
+  './img/github_finder.png',
+  './img/counter.png',
+  './img/animated_nav.png',
+  './img/random_choice.png',
+	'./img/drink_water.png'
+];
+
+const texts = [
+  'Gihub Finder',
+  'Incrementing Counter',
+  'Animated Navigation',
+  'Random Choice Picker',
+	'How many Drinks?'
+];
+
+window.addEventListener('scroll', checkBoxes);
+
+checkBoxes();
+
+function checkBoxes() {
+  const trigger = (window.innerHeight / 5) * 2;
+
+  boxes.forEach((i, index) => {
+    i.style.backgroundImage = `url(${images[index]})`;
+    i.style.backgroundSize = 'contain';		
+
+    i.setAttribute('href', `${links[index]}`);
+    i.innerHTML = `<h3 >${texts[index]}</h3>`;
+    const boxTop = i.getBoundingClientRect().top;
+
+    if (boxTop < trigger) {
+      i.classList.add('show');
+    } else {
+      i.classList.remove('show');
+    }
+  });
+}
