fd318f25401f3d940e09c4b86d0181c8180517c3
SilvesterSpath <51899620+SilvesterSpath@users.noreply.github.com>
2021-05-21 06:23:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,28 +2,30 @@ const boxes = document.querySelectorAll('.box');
 
 const links = [
   'https://silvesterspath.github.io/github_finder/',
+  'https://silvesterspath.github.io/MovieTime/',
   'https://silvesterspath.github.io/Incrementing-Counter/',
   'https://silvesterspath.github.io/Animated-Navigation/',
   'https://silvesterspath.github.io/Random-Picker/',
   'https://silvesterspath.github.io/How-many-Drinks/',
-  'repa',
   'retek',
 ];
 
 const images = [
   './img/github_finder.png',
+  './img/movie.png',
   './img/counter.png',
   './img/animated_nav.png',
   './img/random_choice.png',
-	'./img/drink_water.png'
+  './img/drink_water.png',
 ];
 
 const texts = [
   'Gihub Finder',
+  'Find Your Movies',
   'Incrementing Counter',
   'Animated Navigation',
   'Random Choice Picker',
-	'How many Drinks?'
+  'How many Drinks?',
 ];
 
 window.addEventListener('scroll', checkBoxes);
@@ -35,7 +37,7 @@ function checkBoxes() {
 
   boxes.forEach((i, index) => {
     i.style.backgroundImage = `url(${images[index]})`;
-    i.style.backgroundSize = 'contain';		
+    i.style.backgroundSize = 'contain';
 
     i.setAttribute('href', `${links[index]}`);
     i.innerHTML = `<h3 >${texts[index]}</h3>`;
